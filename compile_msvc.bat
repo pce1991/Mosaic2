e98e805106bcedd1b9caaa90ecbb207906d454ce
pce1991 <patrick.collin.eye@gmail.com>
2025-07-14 13:32:18 -0500

    compiling with msvc now

--- compile_msvc.bat
+++ compile_msvc.bat
@@ -21,7 +21,8 @@ set "BUILD_DIR=build"
 :: === RAYLIB PATHS ===
 set "RAYLIB_PATH=C:\raylib\raylib"
 set "RAYLIB_INCLUDE=%RAYLIB_PATH%\src"
-set "RAYLIB_LIB=%RAYLIB_PATH%\src"
+set "RAYLIB_LIB=C:\raylib\raylib\build_msvc\raylib"
+::set "RAYLIB_LIB=%RAYLIB_PATH%\src"
 
 :: === CREATE BUILD DIRECTORY IF NEEDED ===
 if not exist "%BUILD_DIR%" mkdir "%BUILD_DIR%"
@@ -34,10 +35,15 @@ echo ------------------------
 echo Compiling with MSVC
 
 :: Compile with MSVC, include debug symbols (/Zi), and create PDB (/Fd), enable debug runtime (/MDd or /MTd)
-cl.exe /EHsc /Zi /std:c++20 /MDd /Fe"%BUILD_DIR%\%NAME_PART%.exe" ^
+rem cl.exe /EHsc /Zi /std:c++20 /MDd /Fe"%BUILD_DIR%\%NAME_PART%.exe" ^
+rem     "%INPUT_FULL%" ^
+rem     /I"%RAYLIB_INCLUDE%" /I"external" ^
+rem     /link /LIBPATH:"%RAYLIB_LIB%" raylib.lib opengl32.lib gdi32.lib winmm.lib /DEBUG
+
+cl.exe /EHsc /Zi /std:c++20 /MDd /Fe"%BUILD_DIR%\%NAME_PART%.exe" /Fd"%BUILD_DIR%\%NAME_PART%.pdb" ^
     "%INPUT_FULL%" ^
     /I"%RAYLIB_INCLUDE%" /I"external" ^
-    /link /LIBPATH:"%RAYLIB_LIB%" raylib.lib opengl32.lib gdi32.lib winmm.lib /DEBUG
+    /link /LIBPATH:"%RAYLIB_LIB%" raylib.lib opengl32.lib gdi32.lib winmm.lib user32.lib shell32.lib setupapi.lib ole32.lib advapi32.lib /DEBUG
 
 echo.
 echo Done. Output: %BUILD_DIR%\%NAME_PART%.exe
