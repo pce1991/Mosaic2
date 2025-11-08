7bf46b3197172d456e752b7d81e7cc60f66a4451
pce1991 <patrick.collin.eye@gmail.com>
2025-11-08 17:02:47 -0600

    notes

--- src/math/rand.h
+++ src/math/rand.h
@@ -1,6 +1,11 @@
 
-#define LCG_MULT 1664525u
-#define LCG_INC 1013904223u
+// found that Microsoft used this for C++ lib in 2005, seems fine
+#define LCG_MULT 214013u
+#define LCG_INC 2531011u
+
+// an alternative
+// #define LCG_MULT 1664525u
+// #define LCG_INC 1013904223u
 
 typedef struct {
     uint32 seed;
@@ -20,7 +25,9 @@ uint32 Randi() {
 }
 
 uint32 Randi(uint32 upperLimit) {
-  return Randi() % upperLimit;
+  // we use the high-bits we get from Randi which have better distribution
+  // and then map those onto our correct range.
+  return (uint32)(((uint64)Randi() * upperLimit) >> 32);
 }
 
 int32 RandiRange(int32 lowerLimit, int32 upperLimit) {
@@ -38,3 +45,35 @@ float32 RandfUpper(float32 upperLimit) {
 float32 RandfRange(float32 lowerLimit, float32 upperLimit) {
     return lowerLimit + RandfUpper(upperLimit - lowerLimit);
 }
+
+
+void SeedRand(LCGState *state, uint32 seed) {
+    state->seed = seed;
+    state->state = seed;
+}
+
+uint32 Randi(LCGState *state) {
+    state->state = state->state * LCG_MULT + LCG_INC;
+    return state->state;
+}
+
+uint32 Randi(LCGState *state, uint32 upperLimit) {
+  return Randi(state) % upperLimit;
+}
+
+int32 RandiRange(LCGState *state, int32 lowerLimit, int32 upperLimit) {
+  return lowerLimit + (int32)Randi(state, (uint32)(upperLimit - lowerLimit));
+}
+
+float32 Randf(LCGState *state) {
+  return (float32)Randi(state) / (float32)UINT32_MAX;
+}
+
+float32 RandfUpper(LCGState *state, float32 upperLimit) {
+    return Randf(state) * upperLimit;
+}
+
+float32 RandfRange(LCGState *state, float32 lowerLimit, float32 upperLimit) {
+  return lowerLimit + RandfUpper(state, upperLimit - lowerLimit);
+}
+
